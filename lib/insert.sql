5173029074dc91a5ec6709ac57736246c4d9a1fd
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-29 13:16:30 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)VALUES ('Mr. Chololate', 1, 'male', 'pink', 'goofy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)VALUES ('Rowdy', 1, 'male', 'pink', 'grumpy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)VALUES ('Tabitha', 4, 'F', 'grey', 'angry', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)VALUES ('Sergeant Brown', 1, 'male', 'pink', 'happy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)VALUES ('Melissa', 3, 'female', 'yellow', 'lazy', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)VALUES ('Grinch', 1, 'male', 'pink', 'lazy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)VALUES ('Wendy', 5, 'female', 'orange', 'peppy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)VALUES (NULL, 1, 'male', 'blue', 'aggressive', 0);
